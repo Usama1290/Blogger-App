62966ac45c1e4a5c7a527a82d5f34f9960e9f8d4
TUBA-saleem <tubasaleem833@gmail.com>
2026-01-23 17:46:20 +0500

    FEW UPDATES IN APIS

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cookieParser());
-app.use(express.static(path.resolve("./BlogImages")))
+app.use('/BlogImages', express.static(path.join(__dirname, 'BlogImages')));
 
 
 app.use("/user",userRoutes)
